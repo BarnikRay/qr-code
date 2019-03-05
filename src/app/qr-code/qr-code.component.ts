6c1564b45c94368c91a6e7b38092ea0ac9abc03b
BarnikRay <barnikr06@gmail.com>
2019-03-05 21:21:04 +0530

    minor code change

--- src/app/qr-code/qr-code.component.ts
+++ src/app/qr-code/qr-code.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-qr-code',
@@ -6,11 +7,23 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./qr-code.component.css']
 })
 export class QrCodeComponent implements OnInit {
+  text:string;
+  email:string;
+  number:string;
+  msg:string;
+  subject:string;
+  fname:string;
+  lname:string;
+  option:string;
 
   constructor() {
   }
 
   ngOnInit() {
+
+  }
+  formOnSubmit(form:NgForm){
+    console.log(form.value);
   }
 
 }
