adeb4e189aa9c30b41fcab46ded5d49e2d7394ee
BarnikRay <barnikr06@gmail.com>
2019-04-14 10:04:42 +0530

    directory refactor

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to qr-bar-code!');
+    expect(page.getTitleText()).toEqual('Welcome to qr-code!');
   });
 
   afterEach(async () => {
