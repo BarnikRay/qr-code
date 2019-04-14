f438fe1b223492c84bb97b7b78264dcb77977a61
BarnikRay <barnikr06@gmail.com>
2019-04-14 10:27:33 +0530

    code refactor

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'qr-bar-code'`, () => {
+  it(`should have as title 'qr-code'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('qr-code');
