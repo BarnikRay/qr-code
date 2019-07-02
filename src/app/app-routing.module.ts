aade79c0bb6941ea02e203ab1acf2546eb5bcb74
BarnikRay <barnikr06@gmail.com>
2019-07-02 10:59:30 +0530

    remove routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,11 @@ import {Routes, RouterModule} from '@angular/router';
 import {QrCodeComponent} from './qr-code/qr-code.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'/qr-code',pathMatch:'full'},
-  {path: 'qr-code', component: QrCodeComponent},
-
+  {path: '', component: QrCodeComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{useHash:true})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
